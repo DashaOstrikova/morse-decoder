61f1e4748f6849f503682e924738ab24841a0939
Dasha Ostrikova <dashka.ostrikova@gmail.com>
2022-02-17 23:08:02 +0300

    feat: solve morse decoder

--- src/index.js
+++ src/index.js
@@ -38,8 +38,30 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+    expr = expr.split('**********');
+    let result = [];
+    for (let j = 0; j < expr.length; j++) {
+        let word = expr[j].split('');
+        let word_decode = '';
+        while(word.length) {
+            let morse = '';
+            let letter = word.slice(0, 10);
+            word.splice(0, 10);
+            for (let i = 0; i < 10; i += 2) {
+                if (letter[i] == '1' && letter[i+1] == '0') {
+                    morse += '.';
+                }
+                else if (letter[i] == '1' && letter[i+1] == '1') {
+                    morse += '-';
+                }
+            }
+            word_decode += MORSE_TABLE[morse];
+        }
+        result.push(word_decode);
+    }
+    return result.join(' ');
 }
+
 
 module.exports = {
     decode
